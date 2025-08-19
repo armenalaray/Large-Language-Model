3f1d4a74c762a384780ed0e26ef84f0d40442897
Alejandro Armenta <armenalaray@gmail.com>
2025-08-18 18:31:32 -0600

    Added GPU

--- a.py
+++ a.py
@@ -89,5 +89,31 @@ tokenizer = SimpleTokenizerV2(vocab)
 
 print(tokenizer.decode(tokenizer.encode(text)))
 
+################################################
+#OTHER
+
+from importlib.metadata import version
+import tiktoken
+
+print("tiktoken version:", version("tiktoken"))
+
+tokenizer = tiktoken.get_encoding("gpt2")
+
+with open("the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+
+enc_text = tokenizer.encode(raw_text)
+
+print(len(enc_text))
+
+enc_sample = enc_text[50:]
+
+context_size = 4
+
+for i in range(1,context_size+1):
+    context = enc_sample[:i]
+    desired = enc_sample[i]
+
+    print(context, "----->", desired)
 
 
