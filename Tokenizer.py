8f995da90ae294086e5d5353ed80dc19f577285a
Alejandro Armenta <armenalaray@gmail.com>
2025-08-20 12:53:26 -0600

    -

--- Tokenizer.py
+++ Tokenizer.py
@@ -162,9 +162,35 @@ class SelfAttention_v1(nn.Module):
     
 
 
+torch.manual_seed(123)
+sa_v1 = SelfAttention_v1(d_in, d_out)
+print(sa_v1(inputs))
 
+class SelfAttention_v2(nn.Module):
+    def __init__(self, d_in, d_out, qkv_bias=False):
+        super().__init__()
 
+        #ya no es afin
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
 
+    def forward(self, x):
+        #6*3 3*2
+        #x @ self.W_key
 
+        keys = self.W_key(x)
+        values = self.W_value(x)
+        queries = self.W_query(x)
 
+        attn_scores = queries @ keys.T
 
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1] ** 0.5, dim=-1)
+
+        context_vec = attn_weights @ values
+
+        return context_vec
+
+torch.manual_seed(789)
+sa_v2 = SelfAttention_v2(d_in, d_out)
+print(sa_v2(inputs))
