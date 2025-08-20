0e56e17f3f855b0fd06a8872ba1a8958cfc94504
Alejandro Armenta <armenalaray@gmail.com>
2025-08-19 22:52:25 -0600

    made Self Attention class

--- Tokenizer.py
+++ Tokenizer.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 import tiktoken
 
@@ -109,15 +110,18 @@ W_value = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
 #1*2
 query_2 = x_2 @ W_query
 
+queries = inputs @ W_query
 keys = inputs @ W_key
 values = inputs @ W_value
 
+print("QUERIES:\n",queries)
+print("KEYS:\n",keys.T)
 print("KEYS:\n",keys)
 print("VALUES:\n",values)
 
-attn_score_2 = query_2 @ keys.T
+attn_score = queries @ keys.T
 
-print("ATTN SCORES:\n", attn_score_2)
+print("ATTN SCORES:\n", attn_score)
 
 d_k = keys.shape[-1]
 
@@ -125,13 +129,42 @@ d_k = keys.shape[-1]
 #si attn_score_2 es muy negativo las derivadas van a ser 0
 #attn_weights_2 = torch.softmax(attn_score_2, dim=-1)
 
-attn_weights_2 = torch.softmax(attn_score_2 / d_k ** 0.5, dim=-1)
+attn_weights = torch.softmax(attn_score / d_k ** 0.5, dim=-1)
 
-print("ATTN WEIGHTS:\n",attn_weights_2)
+print("ATTN WEIGHTS:\n",attn_weights)
 
-context_vec_2 = attn_weights_2 @ values
+context_vec_2 = attn_weights @ values
 
 print("CONTEXT_VEC:\n",context_vec_2)
+
+
+class SelfAttention_v1(nn.Module):
+    def __init__(self, d_in, d_out):
+        super().__init__()
+        self.W_query = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_key = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_value = nn.Parameter(torch.rand(d_in, d_out))
+
+    def forward(self, x):
+        #x son los embeddings
+        keys = x @ self.W_key
+        values = x @ self.W_value
+        queries = x @ self.W_query
+
+        attn_scores = queries @ keys.T
+
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1] ** 0.5, dim=-1)
+
+        #estas haciendo combinaciones lineales de rows
+        context_vec = attn_weights @ values
+
+        return context_vec
+    
+
+
+
+
+
 
 
 
