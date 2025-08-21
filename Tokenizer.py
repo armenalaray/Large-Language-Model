d250d1b26955cff3b046c57fd6ab072551ec8dc8
Alejandro Armenta <armenalaray@gmail.com>
2025-08-21 14:48:02 -0600

    Finished Model

--- Tokenizer.py
+++ Tokenizer.py
@@ -30,13 +30,25 @@ class DummyGPTModel(nn.Module):
 
         self.out_head = nn.Linear(cfg["emb_dim"], cfg["vocab_size"], bias=False)
 
-        
-
-
-
     def forward(self, in_idx):
         batch_size, seq_len = in_idx.shape
         tok_embeds = self.tok_emb(in_idx)
+        pos_embeds = self.pos_emb(torch.arange(seq_len, device=in_idx.device))
+
+        x = tok_embeds + pos_embeds
+
+        x = self.drop_emb(x)
+
+        #Las tengo que hacer
+        x = self.trf_blocks(x)
+        x = self.final_norm(x)
+
+        logits = self.out_head(x)
+
+        return logits
+
+
+
         
 
 class DummyTransformerBlock(nn.Module):
@@ -66,13 +78,14 @@ batch.append(torch.tensor(tokenizer.encode(txt2)))
 
 batch = torch.stack(batch, dim=0)
 
+print(batch.shape)
 print(batch)
 
 torch.manual_seed(123)
 model = DummyGPTModel(GPT_CONFIG_124M)
 
-"""
 logits = model(batch)
+print(logits.shape)
 print(logits)
-"""
+
         
