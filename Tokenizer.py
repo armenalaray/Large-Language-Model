9b3434744401d645c5ee305ffecbcaca4d1a4aea
Alejandro Armenta <armenalaray@gmail.com>
2025-08-21 22:58:13 -0600

    -

--- Tokenizer.py
+++ Tokenizer.py
@@ -135,3 +135,28 @@ layer_sizes = [3, 3, 3, 3, 3, 1]
 sample_input = torch.tensor([[1.0, 0.0, -1.0]])
 torch.manual_seed(123)
 modelWithoutShortcut = ExampleDeepNeuralNetwork(layer_sizes, use_shortcut=False)
+
+def print_gradients(model, x):
+    output = model(x)
+    print(output)
+
+    target = torch.tensor([[0.]])
+
+    loss = nn.MSELoss()
+
+    loss = loss(output, target)
+
+    loss.backward()
+
+    for name, param in model.named_parameters():
+        if 'weight' in name:
+            print(f"{name} has gradient mean of {param.grad.abs().mean().item()}")
+        
+
+
+print_gradients(modelWithoutShortcut, sample_input)
+
+torch.manual_seed(123)
+model_with_shortcut = ExampleDeepNeuralNetwork(layer_sizes, use_shortcut=True)
+
+print_gradients(model_with_shortcut, sample_input)
